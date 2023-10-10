23fe5e2b37c9f83025de57abaa1934a60f9498be
Jason <jasonzhang501@gmail.com>
2023-10-10 17:20:32 -0400

    End of P3

--- phase-3-iteration.js
+++ phase-3-iteration.js
@@ -1,1 +1,23 @@
-// Your code here
+Array.prototype.bubbleSort = function() {
+    let sorted = false;
+
+    while (!sorted) {
+        sorted = true;
+        for(let i = 0; i < this.length; i++) {
+            if (this[i] > this[i + 1]) {
+                sorted = false;
+                let temp = this[i + 1];
+                this[i + 1] = this[i]; 
+                this[i] = temp;
+            }
+        }
+    }
+
+    return this
+};
+
+[2,3,7,8,4].bubbleSort();
+
+String.prototype.substrings = function() {
+    let result = []
+}
